5de9692b964b97532bc1fd592f494d32eb53e7b0
vicretana2426 <vicetana2426@gmail.com>
2024-01-26 18:12:21 -0800

    testing

--- Sandboxes/Sandbox_Victor.py
+++ Sandboxes/Sandbox_Victor.py
@@ -3,27 +3,7 @@
 '''This module will be used for debugging, feel free
 to play around here as much as you would like !!'''
 
-import sys
-sys.path.insert(1, "./")
-import Analysis.FirstOrderCalcs as FAC
-import Toolbox.Constant as const
-args = {
-        'thrust': 5000 * const.lbToN,  # Newtons
-        'time': 30,  # s
-        # 'rho_ox' : 1141, #Kg/M^3
-        # 'rho_fuel' : 842,
-        'pc': 350 * const.psiToPa,
-        'pe': 14.7 * const.psiToPa,
-        'phi': 1,
-        'fuelname': 'Ethanol',
-        'oxname': 'LOX',
-        'throat_radius_curvature': .02}
-
-params = FAC.SpreadsheetSolver(args)
-
-
-for i in params:
-    print(i, params[i])
+"hello everyone"
 
 
 
