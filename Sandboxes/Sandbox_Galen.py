e63caf811f4b45b3239729cb1ab58b2bde9530c8
gbh2022UW <gbh2022@uw.edu>
2024-02-21 18:00:10 -0800

    edits to sandbox

--- Sandboxes/Sandbox_Galen.py
+++ Sandboxes/Sandbox_Galen.py
@@ -1,11 +1,12 @@
 "This is for Galen"
 
-mySave = r"/mnt/c/Users/gbhof/Documents/ROOPERT-Save-Files"
-import getpass
+import sys
+sys.path.insert(1,"./")
 import NewOOPSystem.DataManager as DM
 import os
 
-for root, dirs, files in os.walk(".", topdown = False):
+os.chdir(DM.saveDirectory)
+for root, dirs, files in os.walk(".", topdown = True):
     for name in files:
         print(os.path.join(root, name))
     for name in dirs:
